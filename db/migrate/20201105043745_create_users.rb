f41a312a2671e296c06cd85ee1209ae7418183a6
KaladaJumbo <wkalada1@gmail.com>
2020-11-05 00:31:38 -0500

    postgres tested and works

--- db/migrate/20201105043745_create_users.rb
+++ db/migrate/20201105043745_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :first_name
-      t.string :last_name
+      t.text :username
+      t.text :first_name
+      t.text :last_name
 
       t.timestamps
     end
