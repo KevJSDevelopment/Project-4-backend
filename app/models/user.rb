cf32496997f7310e6f605ede55202c59a9769be3
KevJSDevelopment <Kshhn12@gmail.com>
2020-11-06 12:12:59 -0500

    testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
     has_many :messages
     has_many :channels, -> { distinct },through: :messages
 
+    #testing
+
     validates :username, uniqueness: true, presence: true
 end
