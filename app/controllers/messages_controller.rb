66cba474038568742c04c53df3a692e54e5f5e5a
KevJSDevelopment <Kshhn12@gmail.com>
2020-11-05 19:39:59 -0500

    confirmed sending messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,16 @@
 class MessagesController < ApplicationController
 
-
     def create
         # ActionCable.server.broadcast "channel_channel", body: params[:message] 
 
-        message = Message.new(text: params[:message], user_id: 1, channel_id: 2)
+        message = Message.new(text: params[:message], user_id: 1, channel_id: 1)
 
         if message.save
+            # byebug
             channel = Channel.first()
             ChannelChannel.broadcast_to(channel, message)
         else
+            # byebug
             message = "sucks to suck"
             render json: message.json()
         end
