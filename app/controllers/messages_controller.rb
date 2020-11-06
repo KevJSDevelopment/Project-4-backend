416532f0cb284a40f52f4fdf98861fad47b1d46e
KaladaJumbo <wkalada1@gmail.com>
2020-11-06 12:42:40 -0500

    fixed sending errors

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,12 +2,13 @@ class MessagesController < ApplicationController
 
     def create
         # ActionCable.server.broadcast "channel_channel", body: params[:message] 
-        message = Message.new(text: params[:message], user_id: 1, channel_id: 1)
+        message = Message.new(text: params[:message], user_id: User.first().id, channel_id: Channel.first().id)
+        # byebug
 
         if message.save
             # byebug
             channel = Channel.first()
-            ChannelChannel.broadcast_to(channel,  {channel: channel, users: channel.users, message: message})
+            ChannelChannel.broadcast_to(channel, {channel: channel, users: channel.users, message: message, sent: message.user})
         else
             # byebug
             message = "sucks to suck"
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
     has_many :messages
     has_many :channels, -> { distinct },through: :messages
 
-    #testing
 
     validates :username, uniqueness: true, presence: true
 end
